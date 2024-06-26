8665981077ceab1497a60791f68d8ece24685f1c
PRINCEKK122 <awuahkarikari12@gmail.com>
2024-06-26 22:10:25 +0000

    implement student class and getAverageGrade method

---
+++ src/main/Student.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Student {
+    private final List<Double> grades;
+
+    public Student(List<Double> grades) {
+        this.grades = grades;
+    }
+
+    public double getAverageGrade() {
+        double total = 0.0;
+
+        for (double grade : grades) {
+            total += grade;
+        }
+
+        return total;
+    }
+}
